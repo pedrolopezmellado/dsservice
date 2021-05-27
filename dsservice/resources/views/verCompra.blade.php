3b86de5036d372c9b012f9fdf35c80e56da53185
jose <josealberolaat@gmail.com>
2021-05-27 17:58:57 +0200

    estilos ver compra, mis compras y listaServicios

--- dsservice/resources/views/verCompra.blade.php
+++ dsservice/resources/views/verCompra.blade.php
@@ -10,11 +10,8 @@
 <div class="head">
   <div style="margin-left: 250px; margin-top: 30px;">  
       <a href ="{{ action('HomeController@myPurchases') }}">
-        <img src="{{asset('images/cerrar.png') }}" width="40px" height="40px">  
+        <img src="{{asset('images/cerrar.png') }}" width="30px" height="25px">  
       </a>
-  </div>
-  <div class="titulo">
-    <h1>Servicios adquiridos</h1>
   </div>
 </div>
 @endsection
@@ -41,13 +38,13 @@ input::-webkit-inner-spin-button {
   }
 
 .comentario {
-  width: 140%;
-  height: 150px;
-  box-sizing: border-box;
-  border: 2px solid #ccc;
+  margin-top: 40px;
+  margin-left: -170px;
+  width: 750px;
+  height: 120px;
+  border: 1px solid #ccc;
   border-radius: 2px;
-  background-color: #f8f8f8;
-  resize: none;
+  background-color: white;
 }
 
 /* Firefox */
@@ -55,21 +52,21 @@ input[type=number] {
   -moz-appearance: textfield;
 }
 .container {
-        width: 65%;
-        margin: 0 auto;
-        padding: 20px;
-    }
+  width: 65%;
+  margin: 0 auto;
+  padding: 20px;
+}
 
-    .lateral {
- width: 20%;  /* Este será el ancho que tendrá tu columna */
- float:left; /* Aquí determinas de lado quieres quede esta "columna" */
- height:200px;
+.lateral {
+  width: 20%;  /* Este será el ancho que tendrá tu columna */
+  float:left; /* Aquí determinas de lado quieres quede esta "columna" */
+  height:200px;
 }
 
 .principal {
  width: 78%;
  float: right;
- border-left:cyan 2px solid; /* ponemos un donde para que se vea bonito */
+ border-left:#1eaaf1 2px solid; /* ponemos un donde para que se vea bonito */
  height:200px;
 }
 
@@ -114,108 +111,54 @@ input[type=number] {
   color: gray;
 }
 
-
 .button {
-  background-color: #565656;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  margin: 4px 2px;
+  background-color: #5e5e5e;
   cursor: pointer;
-  margin-left:240px;
+  margin-left:280px;
+  text-decoration: none;
+  width: 110px;
+  height: 40px;
+  font-weight: bold;
+  font-size: 14px;
+  color: white;
+  border: none;
+  border-radius: 2px;
 }
 
 .button:hover {
-  background-color: #a49dff;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  margin: 4px 2px;
-  cursor: pointer;
-  margin-left:240px;
-}
-
-.button:active {
-  background-color: #a49dff;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  margin: 4px 2px;
-  cursor: pointer;
-  margin-left:240px;
+  background-color: #1eaaf1;
 }
 
 .button2 {
-  background-color: #565656;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
   text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  cursor: pointer;
+  text-align: center;
+  width: 110px;
+  height: 40px;
+  font-weight: bold;
+  font-size: 14px;
+  color: white;
+  background-color: #1EAAF1;
+  border: none;
+  border-radius: 2px;
+  margin-top:16px;
 }
 
 .button2:hover {
-  background-color: #a49dff;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  cursor: pointer;
-
-}
-
-.button2:active {
-  background-color: #a49dff;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  cursor: pointer;
+  background-color: #5e5e5e;
 }
 
 .contratado {
-  background-color: #a49dff;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
   text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  font-weight: 900;
-  margin: 4px 2px;
-  cursor: pointer;
-  margin-left:10px;
+  width: 110px;
+  height: 40px;
+  font-weight: bold;
+  font-size: 14px;
+  color: white;
+  background-color: #1EAAF1;
+  border: none;
+  border-radius: 2px;
+  cursor: auto;
 }
-
-
 
 .star-llena {
   background: #a49dff;
@@ -305,108 +248,104 @@ input[type=number] {
 
 </style>
 
-     <div class="container">
+<div class="container">
+  <div class="lateral">
+      @if($purchase->service->user->photo != "")
+      <img class="imgredonda" src="{{ asset($purchase->service->user->photo) }}"/></br> 
+      @else
+      <img class="imgredonda" src="{{asset('images/usuario.png')}}"/></br>
+      @endif 
+      <div class="username">
+      {{ $purchase->service->user->name }}
+      </div>
+    </div>
+    <div class = "principal">
+      <div class = "interior">
 
-        <div class="lateral">
-            @if($purchase->service->user->photo != "")
-            <img class="imgredonda" src="{{ asset($purchase->service->user->photo) }}"/></br> 
+        <div class ="servname">
+          {{ $purchase->service->name }}
+          @for ($i = 1; $i <= 5; $i++)
+            @if ($i <= $purchase->service->valoration)
+              <div class="star-llena"></div>
             @else
-            <img class="imgredonda" src="{{asset('images/usuario.png')}}"/></br>
-            @endif 
-            <div class="username">
-            {{ $purchase->service->user->name }}
-            </div>
-          </div>
-          <div class = "principal">
-            <div class = "interior">
-
-              <div class ="servname">
-                {{ $purchase->service->name }}
-                
-                @for ($i = 1; $i <= 5; $i++)
-                    @if ($i <= $purchase->service->valoration)
-                    <div class="star-llena"></div>
-                    @else
-                    <div class="star-vacia"></div>
-                    @endif
-                @endfor
-
-                </div>
-                <div class ="categoria">
-                {{ $purchase->service->category->name }}
-                </div>
-
-                <div class ="description">
-                {{ $purchase->description }}
-                </div>
-
-                <div class ="description">
-                {{ $purchase->service->direction }}
-                </div>
-
-               
-                <div class="row">
-                 <form method="GET" enctype="multipart/form-data">
-                   @csrf
-                   <input type="hidden" name="purchase" value=" {{ $purchase->id }}  " style="height:35px;">
-
-                  @if ($purchase->status == "accepted")
-                  <button class="button" formaction="{{ action('HomeController@abrirDisputa') }}">Disputa</button>
-                   @endif
-                  <button class="contratado">Contratado</button>
-                  </form>
-                </div>
-
-                <div class ="precio">
-                {{ $purchase->amount }}€
-                <div>
-
-                @if ($purchase->status == "accepted")
-                <div class="row">
-
-                  <b class = "valor">¡Valórame!</b>
-
-                  @for ($i = 1; $i <= 5; $i++)
-                  <div class= "darvalor">
-                  <form method="POST" enctype="multipart/form-data" class = "pull-left">
-                  @csrf
-
-                  <input type="hidden" name="valor" value=" {{ $i }}  " style="height:35px;">
-
-                    @if ($i <= $purchase->valoration)
-                    <button class="star-valorada" formaction="{{ action('HomeController@changeValoration') }}" ></button>
-                    @else
-                    <button class="star-vacia2" formaction="{{ action('HomeController@changeValoration') }}" ></button>
-                    @endif
-                    <input type="hidden" name="ident" value=" {{ $purchase->id }}  " style="height:35px;">
-
-                      </div>
-                    </form>
-                  @endfor
-
-                  <form method="POST" enctype="multipart/form-data" class = "pull-left">
-                  @csrf
-
-                  <input type="hidden" name="ident" value=" {{ $purchase->id }}  " style="height:35px;">
-
-                  <textarea name="comentario"  placeholder="Escriba su comentario..." style="resize:none" class="comentario">
-                  @if($purchase->comentario != "") 
-                  {{$purchase->comentario}}
-                  @endif
-                  </textarea>
-
-                  <button class="button2" formaction="{{ action('HomeController@changeComentario') }}" > Enviar</button>
-
-                  </form>
-
-                </div>
-                @endif
-                
-              </div>
-            </div>
+              <div class="star-vacia"></div>
+            @endif
+          @endfor
         </div>
 
-     </div>
-  
+        <div class ="categoria">
+        {{ $purchase->service->category->name }}
+        </div>
+
+        <div class ="description">
+        {{ $purchase->description }}
+        </div>
+
+        <div class ="description">
+        {{ $purchase->service->direction }}
+        </div>
+
+          
+        <div class="row">
+          <form method="GET" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="purchase" value=" {{ $purchase->id }}  " style="height:35px;">
+
+          @if ($purchase->status == "accepted")
+          <button class="button" formaction="{{ action('HomeController@abrirDisputa') }}">Disputa</button>
+            @endif
+          <button class="contratado">Contratado</button>
+          </form>
+        </div>
+
+        <div class ="precio">
+        {{ $purchase->amount }}€
+        <div>
+
+          @if ($purchase->status == "accepted")
+          <div class="row">
+
+            <b class = "valor">¡Valórame!</b>
+
+            @for ($i = 1; $i <= 5; $i++)
+            <div class= "darvalor">
+              <form method="POST" enctype="multipart/form-data" class = "pull-left">
+              @csrf
+
+              <input type="hidden" name="valor" value=" {{ $i }}  " style="height:35px;">
+
+                @if ($i <= $purchase->valoration)
+                <button class="star-valorada" formaction="{{ action('HomeController@changeValoration') }}" ></button>
+                @else
+                <button class="star-vacia2" formaction="{{ action('HomeController@changeValoration') }}" ></button>
+                @endif
+                <input type="hidden" name="ident" value=" {{ $purchase->id }}  " style="height:35px;">
+
+            </div>
+              </form>
+            @endfor
+          
+          <div>
+            <form method="POST" enctype="multipart/form-data" class = "pull-left">
+              @csrf
+
+              <input type="hidden" name="ident" value=" {{ $purchase->id }}  " style="height:35px;">
+
+              <textarea name="comentario" placeholder="Escriba su comentario..." style="resize:none" class="comentario">
+                @if($purchase->comentario != "") 
+                {{$purchase->comentario}}
+                @endif
+              </textarea>
+              <div style="text-align:center; margin-left:-170px;">
+                <button class="button2" formaction="{{ action('HomeController@changeComentario') }}" > E N V I A R</button>
+              </div>
+            </form>
+          </div>
+          @endif
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
